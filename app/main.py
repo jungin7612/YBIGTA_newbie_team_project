377dd2daad8f98a5c13e5308d97182ff69e5944d
yonseistatking <kimhyenwoon@yonsei.ac.kr>
2025-01-14 19:24:36 +0900

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -5,27 +5,12 @@ import os
 
 from app.user.user_router import user
 from app.config import PORT
-from fastapi.middleware.cors import CORSMiddleware
-
-'''
-rejected test용용 third try
-
-이번에는 될 것인가
-'''
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # 또는 허용할 도메인 리스트
-    allow_credentials=True,
-    allow_methods=["*"],  # 모든 메서드 허용
-    allow_headers=["*"],  # 모든 헤더 허용
-)
-
 static_path = os.path.join(os.path.dirname(__file__), "static")
 app.mount("/static", StaticFiles(directory=static_path), name="static")
 
 app.include_router(user)
 
-if __name__ == "__main__":
+if __name__=="__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=PORT, reload=True)
--- app/user/user_service.py
+++ app/user/user_service.py
@@ -1,44 +1,18 @@
 from app.user.user_repository import UserRepository
 from app.user.user_schema import User, UserLogin, UserUpdate
 
-
 class UserService:
     def __init__(self, userRepoitory: UserRepository) -> None:
         self.repo = userRepoitory
 
     def login(self, user_login: UserLogin) -> User:
-        # 사용자 데이터 로드
-        
-        users_list = self.repo._load_users()
-
-        # 이메일 및 비밀번호 확인 플래그
-        email_signal = False
-        pw_signal = False
-
-        # 사용자 검색
-        for user in users_list.values():  # users_list는 딕셔너리 형태로 로드됩니다
-            if user["email"] == user_login.email and user["password"] != user_login.password:
-                email_signal = True
-            if user["email"] == user_login.email and user["password"] == user_login.password:
-                email_signal = True
-                pw_signal = True
-                # User 객체 생성 및 반환
-                return User(email=user["email"], password=user["password"], username=user["username"])
-
-        # 이메일 또는 비밀번호가 일치하지 않을 경우 예외 처리
-        if not email_signal:
-            raise ValueError("User not found")
-        if not pw_signal:
-            raise ValueError("Invalid password")
-        
-        
+        ## TODO
+        user = None
+        return user
         
     def register_user(self, new_user: User) -> User:
         ## TODO
-        if self.repo.get_user_by_email(new_user.email):
-            raise ValueError("User already exists")
-        self.repo.save_user(new_user)
-        
+        new_user = None
         return new_user
 
     def delete_user(self, email: str) -> User:
