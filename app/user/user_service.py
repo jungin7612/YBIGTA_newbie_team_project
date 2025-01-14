c0493394b6a4795ec52d6427dcf8e00f09d26d3e
wooaje <wjpandadavid@naver.com>
2025-01-14 20:49:29 +0900

    update 기능 구현 완료

--- app/user/user_service.py
+++ app/user/user_service.py
@@ -96,8 +96,8 @@ class UserService:
         users_list = self.repo._load_users()
         if user_update.email not in users_list:
             raise ValueError("User not Found")
-        user = users_list[user_update.email]
-        user["password"] = user_update.new_password
+        user = self.repo.get_user_by_email(user_update.email)
+        user.password = user_update.new_password
         self.repo.save_user(user)
         updated_user = self.repo.get_user_by_email(user_update.email)
         return updated_user
