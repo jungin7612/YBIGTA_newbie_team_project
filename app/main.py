570d7acb17b43bc585a45cb24dff77f7b97324ce
jungin7612 <jungin7612@gmail.com>
2025-01-14 18:13:48 +0900

    first commit

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+import os
+
+from app.user.user_router import user
+from app.config import PORT
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 또는 허용할 도메인 리스트
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 메서드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
+
+static_path = os.path.join(os.path.dirname(__file__), "static")
+app.mount("/static", StaticFiles(directory=static_path), name="static")
+
+app.include_router(user)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=PORT, reload=True)
